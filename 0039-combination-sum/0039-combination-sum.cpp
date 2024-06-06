207197afc6969c7be8e6bcdec19ea0e928a1e8ab
Sahil <94748658+Sahil2802@users.noreply.github.com>
2024-06-06 20:41:04 +0530

    Time: 4 ms (74.48%), Space: 12.4 MB (67.19%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> path;
+        sort(candidates.begin(), candidates.end());
+        combine(candidates, 0, target, path, result);
+        return result;
+    }
+
+    void combine(const vector<int>& candidates, int start, int target, vector<int>& path, vector<vector<int>>& result) {
+        if (target == 0){
+            result.push_back(path);
+            return;
+        }
+        if (target < 0){
+            return;
+        }
+        for (int i = start; i < candidates.size(); ++i){
+            path.push_back(candidates[i]);
+            combine(candidates, i, target - candidates[i], path, result);
+            path.pop_back();
+        }
+    }
+};
