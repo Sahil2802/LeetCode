a1e0abc5c8e2cb786fd697c657737d1ae4d16cba
Sahil <94748658+Sahil2802@users.noreply.github.com>
2024-06-22 11:48:04 +0530

    Time: 79 ms (91.54%), Space: 61.9 MB (57.35%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -3,16 +3,15 @@ public:
     void solve(int start, int &n, int k, vector <int> &temp, vector<vector <int>> &ans){
         // base case
         if (k == 0){
-            ans.push_back(temp);
+            ans.push_back(temp); // Add the current combination to the answer
             return;
         }
 
         for(int i = start; i <= n; i++){
-            temp.push_back(i);
+            temp.push_back(i); // Include the current element in the combination
             solve(i + 1, n, k - 1, temp, ans);
-            temp.pop_back();
+            temp.pop_back(); // Backtrack: remove the last added element to try next combination
         }
-
     }
 
     vector<vector<int>> combine(int n, int k) {
