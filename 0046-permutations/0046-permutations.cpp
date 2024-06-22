8856dbdd7def3433e3fb2bb12e0cf5fdef46b928
Sahil <94748658+Sahil2802@users.noreply.github.com>
2024-06-22 23:55:30 +0530

    Time: 0 ms (100%), Space: 8.7 MB (97.88%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     void solve(vector<vector <int>> &ans, vector<int> &temp, vector<int>& nums){
-        // base case
+        // base case : if tempSize is same as the nums size current permutation is completed
         if(temp.size() == nums.size()){
-            ans.push_back(temp);
-            return;
+            ans.push_back(temp); // add the current permutation to the ans
+            return; // return the curr recursive call
         }
 
         for(int i = 0; i < nums.size(); i++){
+            // if the current element is already in temp, skip the element in the iteration.
             if(find(temp.begin(), temp.end(), nums[i]) != temp.end()){
                 continue;
             }
+            // Add the current element nums[i] to temp
             temp.push_back(nums[i]);
+            // recursive call with the updated temp
             solve(ans, temp, nums);
+            // Backtrack: remove the last element added
             temp.pop_back();
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector <int>> ans;
-        vector<int> temp;
+        vector<int> temp; // temporary vector to store the curr permutations
         solve(ans, temp, nums);
         return ans;
     }
