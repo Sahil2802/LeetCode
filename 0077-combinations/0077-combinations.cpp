4954753f2cf47fd236e583407c765d1d71e40356
Sahil <94748658+Sahil2802@users.noreply.github.com>
2024-06-22 11:30:47 +0530

    Time: 96 ms (61.03%), Space: 106.4 MB (38.05%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(int i, int &n, int k, vector <int> &temp, vector<vector <int>> &ans){
+        // base case
+        if (k == 0){
+            ans.push_back(temp);
+            return;
+        }
+        if (i > n) return;
+
+        temp.push_back(i);
+        solve(i + 1, n, k - 1, temp, ans);
+
+        temp.pop_back();
+        solve(i + 1, n, k, temp, ans);
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector <int>> ans;
+        vector <int> temp;
+        // k is the target combination
+        solve(1, n, k, temp, ans);
+        return ans;
+    }
+};
