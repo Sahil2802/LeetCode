9dd11507fcca28a08faefa6283676591531f2c9d
Sahil <94748658+Sahil2802@users.noreply.github.com>
2024-07-23 15:27:44 +0530

    Time: 95 ms (93.13%), Space: 78.3 MB (88.16%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,47 +1,36 @@
 class Solution {
 public:
-        vector<int> nextSmallerElementIndex(vector<int>& heights,int n){
-        stack<int> st;
-        vector<int> res(n);
-        st.push(-1);
-        for(int i=n-1;i>=0;i--){
-            int curr=heights[i];
-            while(st.top()!=-1 && heights[st.top()]>=curr){
-                st.pop();
-            }
-            res[i]=st.top();
-            st.push(i);
-        }
-        return res;
-    }
-    
-    vector<int> prevSmallerElementIndex(vector<int>& heights,int n){
-        stack<int> st;
-        vector<int> res(n);
-        st.push(-1);
-        for(int i=0;i<n;i++){
-            int curr=heights[i];
-            while(st.top()!=-1 && heights[st.top()]>=curr){
-                st.pop();
-            }
-            res[i]=st.top();
-            st.push(i);
-        }
-        return res;
-    }
-
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int> next=nextSmallerElementIndex(heights,n);
-        vector<int> prev=prevSmallerElementIndex(heights,n);
-        
-        int ans=INT_MIN;
-        for(int i=0;i<n;i++){
-            int len=heights[i];
-            if(next[i]==-1) next[i]=n;
-            int width=next[i]-prev[i]-1;
-            ans=max(ans,len*width);
+        stack<int> st;
+        st.push(-1);
+        int max_area = 0;
+
+        for(int i = 0; i < heights.size(); i++){
+            // While the current bar is shorter than the bar at the stack's top index
+            while (st.top() != -1 && heights[st.top()] >= heights[i]){
+                int height = heights[st.top()];
+                st.pop(); // Remove the top index after copying it
+                int width;
+                // If stack is empty, the width is the current index
+                if (st.empty()) width = i;
+                // Otherwise, the width is the difference between the current index and the new top index minus one
+                else width = i - st.top() - 1;
+                // Calculate the area for the rectangle with the height of the bar at the stack's top index
+                max_area = max(max_area, height * width);
+            }
+            // Push the current index onto the stack
+            st.push(i);
         }
-        return ans;
+
+        // Process any remaining bars in the stack i.e. These elements represent heights for which no smaller heights were found to the right
+        while(st.top() != -1){
+            int height = heights[st.top()];
+            st.pop();
+            int width = heights.size() - st.top() - 1;
+            // Update the maximum area
+            max_area = max(max_area, height * width);
+        }
+
+        return max_area;
     }
 };
