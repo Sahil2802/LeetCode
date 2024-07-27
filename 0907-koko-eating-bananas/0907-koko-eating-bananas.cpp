b7f8451f628affbd941d00ea45a10f560fbeed05
Sahil <94748658+Sahil2802@users.noreply.github.com>
2024-07-27 20:32:15 +0530

    Time: 34 ms (47.47%), Space: 21.7 MB (24.01%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
-        int l = 1;
-        int r = 1;
+        int l = 1; // Minimum possible eating speed
+        int r = 1; // Maximum possible eating speed
+        // find the maximum number of bananas in a single pile
         for(int pile: piles){
             r = max(pile, r);
         }
@@ -11,16 +12,21 @@ public:
         while(l < r){
             int mid = l + (r - l)/2;
             int time = 0;
+            // Time taken to eat all bananas at the current eating speed 
             for(int pile: piles){
-                time += ceil(pile * 1.0/mid);
+                time += ceil(pile * 1.0/mid); // Use ceiling to account for partial hours
             }
+
+            // If the total time is within the allowed hours, adjust the upper bound
             if(time <= h){
-                r = mid;
+                r = mid; // reduce the eating speed
             }
+            // Increase the lower bound since the current speed is too slow
             else{
                 l = mid + 1;
             }
         }
+        // The minimum possible eating speed
         return l;
         
     }
