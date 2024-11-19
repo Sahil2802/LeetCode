8eca9c2c99ca33e045e307317874c11193a69333
Sahil <94748658+Sahil2802@users.noreply.github.com>
2024-11-19 19:19:02 +0530

    Time: 0 ms (100%), Space: 27.7 MB (50.63%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
+    // Function to reverse elements in the array from index 'left' to 'right'
+    void reverse(vector<int> &nums, int left, int right) {
+        int lp = left;
+        int rp = right;
+
+        // Swap elements from the two ends
+        while (lp < rp) {
+            int temp = nums[lp];
+            nums[lp] = nums[rp];
+            nums[rp] = temp;
+            lp++;
+            rp--;
+        }
+    }
+
+    // Function to rotate the array to the right by 'k' positions
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size(); 
-        k = k % n;
-        vector<int> temp(k);
+        int n = nums.size();
+        k = k % n; // Ensure k is within the bounds of the array length
 
-        // store the elements in an temp array
-        for (int i = 0; i < k; i++){
-            temp[i] = nums[n - k + i];
-        }
-
-        // shifting
-        for(int i = n - 1; i >= k; i--){
-            nums[i] = nums[i - k];
-        }
-
-        // push back elements from temp
-        for(int i = 0; i < k; i++){
-            nums[i] = temp[i];
-        }
+        // Reverse the first part of the array (from 0 to n-k-1)
+        reverse(nums, 0, n-k-1);
+        // Reverse the second part of the array (from n-k to n-1)
+        reverse(nums, n-k, n-1);
+        // Reverse the entire array
+        reverse(nums, 0, n-1);
     }
 };
