8008f55504d22f0f7e0974ef7d2c7f765f340ced
Sahil <94748658+Sahil2802@users.noreply.github.com>
2024-12-08 23:44:02 +0530

    Time: 4 ms (80.94%), Space: 128.3 MB (47.43%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> pos; // Vector to store positive numbers
-        vector<int> neg; // Vector to store negative numbers
+        int posIdx = 0, negIdx = 1; // Initialize indices for positive and negative numbers
         int n = nums.size(); 
+        vector<int> ans(n, 0); 
 
         for (int i = 0; i < n; i++) {
             if (nums[i] > 0) {
-                pos.push_back(nums[i]); // Add positive number to pos vector
+                ans[posIdx] = nums[i]; // Place positive number at posIdx
+                posIdx += 2; // Move to the next position for positive number
             } else {
-                neg.push_back(nums[i]); // Add negative number to neg vector
+                ans[negIdx] = nums[i]; // Place negative number at negIdx
+                negIdx += 2; // Move to the next position for negative number
             }
         }
 
-        for (int i = 0; i < n / 2; i++) {
-            nums[i * 2] = pos[i]; // Place positive number at even index
-            nums[i * 2 + 1] = neg[i]; // Place negative number at odd index
-        }
-
-        return nums; 
+        return ans; 
     }
 };
