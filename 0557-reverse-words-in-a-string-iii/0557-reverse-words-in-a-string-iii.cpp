d05a07709f8aa06a364ce29a886b6058d09eb88d
Sahil <94748658+Sahil2802@users.noreply.github.com>
2024-12-17 23:33:13 +0530

    Time: 1 ms (64.37%), Space: 12.2 MB (58.68%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+        int l = 0, r = 0; // Initialize two pointers, l (left) and r (right)
+
+        // Iterate over each character in the input string
+        while (r <= n) {
+            // If we find a space or reach the end of the string, reverse the word
+            if (r == n || s[r] == ' ') {
+                reverse(s.begin() + l, s.begin() + r); // Reverse the current word
+                l = r + 1; // Move the left pointer to the start of the next word
+            }
+            r++; // Move the right pointer to the next character
+        }
+        return s;
+    }
+};
