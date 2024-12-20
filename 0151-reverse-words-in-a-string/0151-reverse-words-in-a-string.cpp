7c621989b7c07646f02b1569947d05d15143d3a6
Sahil <94748658+Sahil2802@users.noreply.github.com>
2024-12-20 20:52:51 +0530

    Time: 0 ms (100%), Space: 10.4 MB (47.71%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string result; // Initialize an empty string to store the result
-        
-        // Iterate through each character in the input string
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == ' ')
-                continue; // Skip spaces
+        if (s.size() == 0) return s; // Return the string if it is empty
 
+        stack<string> st; // Stack to store words
+        string result; // String to store the final result
+
+        // Iterate through each character in the input string
+        for (int i = 0; i < s.size(); i++) {
             string word; // Initialize an empty string to store the current word
 
+            if (s[i] == ' ') continue; // Skip spaces
+
             // Collect the current word
-            while (i < s.size() && s[i] != ' ')
-                word += s[i++]; // Append characters to the word until a space is encountered
-            
-            if (result.empty()) // If this is the first word
-                result = word; // Set the result to the current word
-            else // If this is not the first word
-                result = word + ' ' + result; // Prepend the current word to the result with a space
+            while (i < s.size() && s[i] != ' ') {
+                word += s[i]; // Append characters to the word until a space is encountered
+                i++;
+            }
+
+            st.push(word); // Push the collected word onto the stack
         }
-        
+
+        // Pop words from the stack and construct the result string
+        while (!st.empty()) {
+            result += st.top(); // Append the top word from the stack to the result
+            st.pop(); // Remove the top word from the stack
+
+            if (!st.empty()) result += " "; // Add a space if there are more words to process
+        }
+
         return result; // Return the final result string
     }
 };
