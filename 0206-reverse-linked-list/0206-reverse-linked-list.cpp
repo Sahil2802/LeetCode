a395b0103d627b5bb1ad2e0d416d6b8b74b4e77f
Sahil <94748658+Sahil2802@users.noreply.github.com>
2024-12-23 23:04:10 +0530

    Time: 0 ms (100%), Space: 13.6 MB (7.82%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,16 +11,20 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* current = head;
-        ListNode* prev = NULL, *nextNode = NULL;
-
-        while(current != NULL){
-            nextNode = current->next;
-            current->next = prev;
-            prev = current;
-            current = nextNode;
+        // Base case: if the list is empty or has only one node, return the head
+        if (head == nullptr || head->next == nullptr) {
+            return head;
         }
-        return prev;
-    }
 
+        // Recursively reverse the rest of the list
+        ListNode* newHead = reverseList(head->next);
+
+        // Reverse the current node's pointer
+        ListNode* front = head->next; // Get the next node
+        front->next = head;           // Make the next node point to the current node
+        head->next = nullptr;         // Set the current node's next to nullptr
+
+        // Return the new head of the reversed list
+        return newHead;
+    }
 };
