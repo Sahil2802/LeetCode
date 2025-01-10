ad02b0719586b7da860ab650eb18775a77c0d46c
Sahil <94748658+Sahil2802@users.noreply.github.com>
2025-01-10 23:42:49 +0530

    Time: 0 ms (100%), Space: 14.3 MB (9.68%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int left = 0;
-        int right = nums.size() - 1;
+        int n = nums.size(); // Get the size of the array
+        int low = 0, high = n - 1; // Initialize low and high pointers
+        int ans = INT_MAX; // Initialize the answer to the maximum possible integer value
 
-        while(left < right){
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] > nums[right]){
-                left = mid + 1;
-
+        // Binary search loop
+        while (low <= high) {
+            int mid = low + (high - low) / 2; // Calculate the middle index
+            // if the whole array is sorted
+            if(nums[low] <= nums[high]){
+                ans = min(ans, nums[low]);
+                break;
             }
-            else {
-                right = mid;
+            // Check if the left part is sorted
+            if (nums[low] <= nums[mid]) {
+                // Update the answer with the minimum value found so far
+                ans = min(ans, nums[low]);
+                // Move the low pointer to mid + 1 to search in the right part
+                low = mid + 1;
+            } else {
+                // If the left part is not sorted, the right part must be sorted
+                // Update the answer with the minimum value found so far
+                ans = min(ans, nums[mid]);
+                // Move the high pointer to mid - 1 to search in the left part
+                high = mid - 1;
             }
         }
-        // Minimum element
-        return nums[left];
+        // Return the minimum value found
+        return ans;
     }
 };
