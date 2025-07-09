934cf2cf9470af0c327dc83529a4e9ac4c957e38
Sahil <94748658+Sahil2802@users.noreply.github.com>
2025-07-09 12:24:02 +0530

    Time: 0 ms (100%), Space: 14.5 MB (65.54%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,48 +1,53 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
-        // Get the dimensions of the board
         int m = board.size();
         int n = board[0].size();
 
-        // Define the 4 possible directions for movement (up, down, left, right)
-        vector<pair<int,int>> dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+        queue<pair<int, int>> q;
 
-        // Traverse the first and last columns to mark connected 'O's
-        for (int i = 0; i < m; ++i) {
-            if (board[i][0] == 'O') dfs(i, 0, board, m, n, dirs); // First column
-            if (board[i][n-1] == 'O') dfs(i, n-1, board, m, n, dirs); // Last column
-        }
-
-        // Traverse the first and last rows to mark connected 'O's
-        for (int j = 0; j < n; ++j) {
-            if (board[0][j] == 'O') dfs(0, j, board, m, n, dirs); // First row
-            if (board[m-1][j] == 'O') dfs(m-1, j, board, m, n, dirs); // Last row
-        }
-
-        // Update the board
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                // Convert unmarked 'O's to 'X'
-                if (board[i][j] == 'O') board[i][j] = 'X';
-                // Convert temporary marked 'T's back to 'O'
-                if (board[i][j] == 'T') board[i][j] = 'O';
+        for(int i = 0; i < n; i++){
+            if(board[0][i] == 'O'){
+                q.push({0, i});
+                board[0][i] = 'T';
+            } 
+            if(board[m-1][i] == 'O'){
+                q.push({m-1, i});
+                board[m-1][i] = 'T';
             }
         }
-    }
-    
-private:
-    // Depth-First Search (DFS) to mark connected 'O's
-    void dfs(int r, int c, vector<vector<char>>& board, int m, int n, vector<pair<int,int>>& dirs) {
-        // Boundary check and character comparison
-        if (r < 0 || r >= m || c < 0 || c >= n || board[r][c] != 'O') return;
+        for(int j = 0; j < m; j++){
+            if(board[j][0] == 'O'){
+                q.push({j, 0});
+                board[j][0] = 'T';
+            } 
+            if(board[j][n - 1] == 'O'){
+                q.push({j, n-1});
+                board[j][n-1] = 'T';
+            }
+        }
 
-        // Mark the cell as visited by changing 'O' to 'T'
-        board[r][c] = 'T';
+        vector<pair<int, int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
+        while(!q.empty()){
+            auto [r,c] = q.front();
+            q.pop();
 
-        // Explore all 4 directions
-        for (auto [dr, dc] : dirs) {
-            dfs(r + dr, c + dc, board, m, n, dirs);
+            for(auto [dr,dc] : directions){
+                int nr = r + dr;
+                int nc = c + dc;
+
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n && board[nr][nc] == 'O'){
+                    q.push({nr, nc});
+                    board[nr][nc] = 'T';
+                }
+            }
+        }
+
+        for(int r = 0; r < m; r++){
+            for(int c = 0; c < n; c++){
+                if(board[r][c] == 'O') board[r][c] = 'X';
+                if(board[r][c] == 'T') board[r][c] = 'O';
+            }
         }
     }
 };
