16e1aa57a7821e5e44a6d4c1bd5ab92ac9a9656d
Sahil <94748658+Sahil2802@users.noreply.github.com>
2025-07-17 14:47:45 +0530

    Time: 0 ms (100%), Space: 17.3 MB (89.84%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -7,40 +7,35 @@ public:
 
         // Check each component of the graph
         for(int i = 0; i < n; i++){
-            // If the node is not colored, perform BFS from that node
+            // If the node is not colored, perform DFS from that node
             if(color[i] == -1){
-                if(bfs(i, graph, color) == false) // If not bipartite, return false
+                // If DFS finds a conflict, graph is not bipartite
+                if(dfs(i, 0, graph, color) == false)
                     return false;
             }
         }
         // If all components are bipartite, return true
         return true;
     }
+
 private:
-    // Helper function to perform BFS and check bipartiteness
-    bool bfs(int start, vector<vector<int>>& graph, vector<int> &color){
-        queue <int> q;
-        q.push(start); // Start BFS from the given node
-        color[start] = 0; // Assign the first color
+    // Helper function to perform DFS and check bipartiteness
+    bool dfs(int node, int c, vector<vector<int>>& graph, vector<int> &color){
+        color[node] = c; // Assign color c to the current node
 
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-
-            // Traverse all neighbors of the current node
-            for(int neighbor : graph[node]){
-                if(color[neighbor] == -1){
-                    // If neighbor is not colored, assign alternate color and add to queue
-                    q.push(neighbor);
-                    color[neighbor] = 1 - color[node];
-                }
-                else if(color[neighbor] == color[node]){
-                    // If neighbor has the same color, graph is not bipartite
+        // Traverse all neighbors of the current node
+        for(int neighbor : graph[node]){
+            if(color[neighbor] == -1){
+                // If neighbor is not colored, assign alternate color and continue DFS
+                if(dfs(neighbor, 1 - c, graph, color) == false)
                     return false;
-                }
+            }
+            else if(color[neighbor] == color[node]){
+                // If neighbor has the same color, graph is not bipartite
+                return false;
             }
         }
-        // If BFS completes without conflicts, the component is bipartite
+        // If DFS completes without conflicts, the component is bipartite
         return true;
     }
 };
