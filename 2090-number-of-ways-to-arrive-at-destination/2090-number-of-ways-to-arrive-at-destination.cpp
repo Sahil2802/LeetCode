c2f02a963c372ab5039edf7657521702b2fad8c1
Sahil <94748658+Sahil2802@users.noreply.github.com>
2025-11-19 13:39:03 +0530

    Time: 7 ms (67.43%), Space: 38.1 MB (25.04%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
+    // Modulo for counting ways
     const long long MOD = 1e9 + 7;
+
     int countPaths(int n, vector<vector<int>>& roads) {
+        // Build adjacency list: adj[u] = vector of {v, travel_time}
         vector<vector<pair<int, long long>>> adj(n);
         for(auto &road : roads){
             int u = road[0], v = road[1];
@@ -10,15 +13,19 @@ public:
             adj[v].push_back({u , t});
         }
 
+        // dist[i] = shortest distance from node 0 to i
         vector<long long> dist(n, LLONG_MAX);
+        // ways[i] = number of shortest paths from node 0 to i
         vector<long long> ways(n, 0);
 
+        // Min-heap (distance, node) to perform Dijkstra
         priority_queue<
             pair<long long, int>,
             vector<pair<long long, int>>,
             greater<pair<long long, int>>
         > pq;
 
+        // start from node 0
         pq.push({0, 0});
         dist[0] = 0;
         ways[0] = 1;
@@ -27,20 +34,27 @@ public:
             auto [d, node] = pq.top();
             pq.pop();
 
+            // If we have already found a better distance, skip
             if(d > dist[node]) continue;
 
+            // Relax edges
             for(auto &[neighbor, wt] : adj[node]){
                 long long newDist = d + wt;
+
+                // Found a strictly shorter path to neighbor
                 if(newDist < dist[neighbor]){
                     dist[neighbor] = newDist;
+                    // Number of ways to reach neighbor equals ways to reach current node
                     ways[neighbor] = ways[node];
                     pq.push({newDist, neighbor});
                 }
+                // Found an additional shortest path (same distance)
                 else if(newDist == dist[neighbor]){
                     ways[neighbor] = (ways[neighbor] + ways[node]) % MOD;
                 }
             }
         }
+        // Return number of shortest paths to node n-1 modulo MOD
         return ways[n-1] % MOD;
     }
 };
