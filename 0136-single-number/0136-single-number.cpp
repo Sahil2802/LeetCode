69e6b12d50c391ad704634af5fe568527c50656d
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-01-13 22:04:31 +0530

    Time: 0 ms (100%), Space: 20.8 MB (25.14%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,21 +3,10 @@ public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
 
-        map<int, int>
-            freq; // Initialize a map to count the frequency of each number
-
-        // Count the occurrences of each number in the map
-        for (int i = 0; i < n; i++) {
-            freq[nums[i]]++;
+        int xorRes = 0;
+        for(int i = 0; i < n; i++){
+            xorRes = xorRes ^ nums[i]; 
         }
-
-        // Find the number that appears only once
-        for (auto it : freq) {
-            if (it.second == 1) {
-                return it.first; // Return the number that appears only once
-            }
-        }
-
-        return 0;
+        return xorRes;
     }
 };
