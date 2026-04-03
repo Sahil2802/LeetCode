4e1bd849726a7de165d4b28e04d2191894add018
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-04-03 11:37:26 +0530

    Time: 83 ms (60.19%), Space: 56.1 MB (72.1%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atMost(nums, k) - atMost(nums, k-1);
+    }
+
+    int atMost(vector<int>& nums, int k){
+        int n = nums.size();
+        int left = 0;
+        int count = 0;
+        
+        unordered_map<int, int> freq;
+        for(int right = 0; right < n; right++){
+            freq[nums[right]]++;
+            while(freq.size() > k){
+                freq[nums[left]]--;
+                if(freq[nums[left]] == 0){
+                    freq.erase(nums[left]);
+                }
+                left++;
+            }
+            count += (right - left + 1);
+        }
+        return count;
+    }
+};
