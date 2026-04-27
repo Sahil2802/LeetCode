146690e3d3898f45b5fe2cbd16bd43ed3be3a2d1
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-04-27 17:14:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -2,31 +2,42 @@ class Solution {
 public:
     string reverseVowels(string s) {
         int n = s.size();
-        int i = 0, j = n-1;
-        while(i < j){
-            if(isVowel(s[i]) && isVowel(s[j])){
+
+        // Two pointers:
+        // i starts from left, j starts from right.
+        int i = 0, j = n - 1;
+
+        // Continue until the pointers cross.
+        while (i < j) {
+            // Case 1: both characters are vowels -> swap and move both pointers.
+            if (isVowel(s[i]) && isVowel(s[j])) {
                 swap(s[i], s[j]);
                 i++;
                 j--;
             }
-            else if(isVowel(s[i])){
+            // Case 2: left is vowel, right is not -> move right pointer left.
+            else if (isVowel(s[i])) {
                 j--;
             }
-            else if(isVowel(s[j])){
+            // Case 3: right is vowel, left is not -> move left pointer right.
+            else if (isVowel(s[j])) {
                 i++;
             }
-            else{
+            // Case 4: neither is vowel -> move both pointers inward.
+            else {
                 i++;
                 j--;
             }
         }
+
+        // String with vowels reversed in-place.
         return s;
     }
 
-    bool isVowel(char c){
-        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-           c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'
-        ){
+    // Helper function to check if a character is a vowel (both lowercase and uppercase).
+    bool isVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
             return true;
         }
         return false;
