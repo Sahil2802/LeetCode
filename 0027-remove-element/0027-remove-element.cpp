c3935b2a88df6b2baff0c1fbf9610b6e49cef049
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-04-28 12:33:13 +0530

    Time: 0 ms (100%), Space: 11.7 MB (83.35%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int k = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] != val){
+        int k = 0;  // pointer to place the next valid (non-val) element
+
+        // iterate through the entire array
+        for (int i = 0; i < nums.size(); i++) {
+
+            // if current element is NOT equal to val
+            if (nums[i] != val) {
+
+                // place it at index k (front part of array)
                 nums[k] = nums[i];
+
+                // move k forward to next position
                 k++;
             }
+            // if nums[i] == val → we skip it (do nothing)
         }
+
+        // k represents the number of valid elements (not equal to val)
         return k;
     }
 };
