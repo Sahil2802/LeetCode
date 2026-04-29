432f5c843724b446c772f5cc1fa0eadb83146f0f
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-04-29 15:32:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
+        // Sort people by weight so we can pair the lightest and heaviest efficiently.
         sort(people.begin(), people.end());
-        int n = people.size();
 
-        int boats = 0;
+        int n = people.size();
         int l = 0, r = n - 1;
-        int weightSum = 0;
-        while(l <= r){
-            weightSum = people[l] + people[r];
-            if(weightSum <= limit){
+        int boats = 0;
+
+        // Keep placing people into boats until all are assigned.
+        while (l <= r) {
+            // Try to pair the lightest and heaviest person.
+            // If their combined weight fits, move the left pointer too.
+            if (people[l] + people[r] <= limit) {
                 l++;
             }
+
+            // The heaviest person always takes a boat, so move right pointer.
             r--;
+
+            // One boat is used in every iteration.
             boats++;
         }
+
         return boats;
     }
 };
