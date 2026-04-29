24ef0055fcd5f73ef3ff94c087eecce23fd43532
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-04-29 14:21:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -3,26 +3,34 @@ public:
     int numSubseq(vector<int>& nums, int target) {
         int n = nums.size();
         int mod = 1e9 + 7;
+
+        // Sort the array so that both ends have min and max.
         sort(nums.begin(), nums.end());
-        
-        vector<int>power(n);
+
+        // power[i] = 2^i 
+        // This precomputed array helps count how many subsequences can be formed between two pointers.
+        vector<int> power(n);
         power[0] = 1;
-        
-        for(int i = 1; i < n; i++){
-            power[i] = (power[i-1]*2)%mod;
+        for (int i = 1; i < n; i++) {
+            power[i] = (power[i - 1] * 2) % mod;
         }
 
         int result = 0;
         int left = 0, right = n - 1;
-        while(left <= right){
-            if(nums[left] + nums[right] <= target){
-                result = (result + power[right - left])%mod;
+
+        // Pair the smallest and largest elements.
+        while (left <= right) {
+            // If nums[left] + nums[right] is valid,
+            // then every subset of the elements between them is also valid.
+            if (nums[left] + nums[right] <= target) {
+                result = (result + power[right - left]) % mod;
                 left++;
-            }
-            else{
+            } else {
+                // Sum is too large, so reduce the larger element.
                 right--;
             }
         }
+
         return result;
     }
 };
