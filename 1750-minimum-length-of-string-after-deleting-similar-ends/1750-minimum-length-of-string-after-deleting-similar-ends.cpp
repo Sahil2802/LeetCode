10050d6a93585a41738e3222058d2626db19f238
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-05-01 13:42:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -4,11 +4,23 @@ public:
         int n = s.size();
 
         int l = 0, r = n - 1;
-        while(l < r && s[l] == s[r]){
+
+        // Shrink from both ends while the end characters match.
+        while (l < r && s[l] == s[r]) {
             char ch = s[l];
-            while(l < r && s[l] == ch) l++;
-            while(l <= r && s[r] == ch) r--;
+
+            // Remove matching characters from the left.
+            while (l < r && s[l] == ch) {
+                l++;
+            }
+
+            // Remove matching characters from the right.
+            while (l <= r && s[r] == ch) {
+                r--;
+            }
         }
-        return (r-l+1);
+
+        // Remaining length after all deletions.
+        return (r - l + 1);
     }
 };
