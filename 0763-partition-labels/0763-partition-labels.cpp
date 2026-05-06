2b11f21143e3a71a10c26b7c8b02304541fcb288
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-05-07 00:15:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -3,24 +3,27 @@ public:
     vector<int> partitionLabels(string s) {
         int n = s.size();
 
+        // lastSeen[c] = last index where character c appears.
         vector<int> lastSeen(26, -1);
-        for(int i = 0; i < n; i++){
-            int idx = s[i] - 'a';
-            lastSeen[idx] = i;
+        for (int i = 0; i < n; i++) {
+            lastSeen[s[i] - 'a'] = i;
         }
 
-        int i = 0;
-        int start = 0;
-        int end = 0;
         vector<int> result;
-        while(i < n){
-            end = max(lastSeen[s[i] - 'a'], end);
-            if(i == end){
-                result.push_back((end - start + 1));
+        int start = 0;  // start index of current partition
+        int end = 0;    // farthest index needed for current partition
+
+        for (int i = 0; i < n; i++) {
+            // Extend the partition to include all occurrences of s[i].
+            end = max(end, lastSeen[s[i] - 'a']);
+
+            // If we reached the end, finalize this partition.
+            if (i == end) {
+                result.push_back(end - start + 1);
                 start = end + 1;
             }
-            i++;
         }
+
         return result;
     }
 };
