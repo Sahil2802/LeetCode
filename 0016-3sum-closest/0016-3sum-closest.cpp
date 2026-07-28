b0657c345540bba014951bd782ff5d792d47ba0f
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-07-28 13:31:06 +0530

    Time: 11 ms (59.27%), Space: 14 MB (47.91%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); // needed for two-pointer technique
         int n = nums.size();
 
-        int closestSum = nums[0] + nums[1] + nums[2];
+        int closestSum = nums[0] + nums[1] + nums[2]; // initialize with any valid triplet
 
-        for(int i = 0; i < n; i++){
-            int left = i + 1, right = n-1;
-            
-            while(left < right){
+        for (int i = 0; i < n; i++) {
+            int left = i + 1, right = n - 1; // two pointers on the remaining sorted range
+
+            while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if(abs(sum - target) < abs(closestSum - target)){
+                // Update closestSum if this triplet is nearer to target
+                if (abs(sum - target) < abs(closestSum - target)) {
                     closestSum = sum;
                 }
 
-                if(sum == target){
-                    return sum;
-                }
-                else if(sum < target){
-                    left++;
-                }
-                else{
-                    right--;
+                if (sum == target) {
+                    return sum; // exact match, impossible to get closer than this
+                } else if (sum < target) {
+                    left++;  // sum too small, move left pointer right to increase it
+                } else {
+                    right--; // sum too big, move right pointer left to decrease it
                 }
             }
         }
