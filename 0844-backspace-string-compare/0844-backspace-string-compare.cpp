1ccf9498062f408812adb07cb9e62557e05187c7
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-07-30 20:32:21 +0530

    Time: 3 ms (4.57%), Space: 8.9 MB (5.46%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
+        // Compare the final "typed" result of both strings
         return buildFinalString(s) == buildFinalString(t);        
     }
 
-    stack<char> buildFinalString(string str){
+    stack<char> buildFinalString(string str) {
         stack<char> st;
-        for(char ch : str){
-            if(ch == '#'){
-                 if(!st.empty()) st.pop();  // guard: only pop if something exists
-            }
-            else{
+        for (char ch : str) {
+            if (ch == '#') {
+                // '#' means backspace: remove last typed char, if any
+                if (!st.empty()) {
+                    st.pop();
+                }
+                // if stack is empty, backspacing does nothing
+            } else {
+                // regular character: type it
                 st.push(ch);
             }
         }
