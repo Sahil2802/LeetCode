3024ae22f00f8bf6299b83075622cbda090eb62f
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-08-03 21:53:03 +0530

    Time: 0 ms (100%), Space: 12.4 MB (26.16%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;      // pointer to last valid element in nums1
-        int j = n - 1;      // pointer to last element in nums2
-        int k = m + n - 1;  // write pointer, last position of merged array
-
-        // Merge from the back so we never overwrite unprocessed nums1 elements
-        while (j >= 0) {
-            if (i >= 0 && nums1[i] > nums2[j]) {
-                // nums1's current element is bigger, place it at the end
-                nums1[k] = nums1[i];
-                i--;
-            } else {
-                // nums2's current element is bigger (or nums1 exhausted)
-                nums1[k] = nums2[j];
-                j--;
-            }
-            k--;
+        vector<int> merged;
+        
+        // Take valid elements from nums1
+        for (int i = 0; i < m; i++) {
+            merged.push_back(nums1[i]);
         }
-        // If nums1 still has elements left (i >= 0), they're already
-        // smaller than everything placed so far and sit correctly at the front
+        
+        // Take all elements from nums2
+        for (int i = 0; i < n; i++) {
+            merged.push_back(nums2[i]);
+        }
+        
+        // Sort combined array
+        sort(merged.begin(), merged.end());
+        
+        // Copy back into nums1
+        for (int i = 0; i < m + n; i++) {
+            nums1[i] = merged[i];
+        }
     }
 };
