3f6fad505bc8571cc92ca0d2cb9540261a310e41
Sahil <94748658+Sahil2802@users.noreply.github.com>
2026-08-29 18:36:02 +0530

    Time: 0 ms (100%), Space: 7.7 MB (57.13%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     bool judgeSquareSum(int c) {
         long left = 0;
-        long right = (long)sqrt(c);
+        long right = (long)sqrt(c); // right starts at the largest possible value
 
-        while(left <= right){
-            long sum = left * left + right * right;
-            if(sum == c){
-                return true;
+        while (left <= right) {
+            long sum = left * left + right * right; // use long to avoid overflow
+
+            if (sum == c) {
+                return true; // found a valid pair (a=left, b=right)
             }
-            else if(sum > c){
-                right--;
+            else if (sum > c) {
+                right--; // sum too big, decrease it
             }
-            else{
-                left++;
+            else {
+                left++; // sum too small, increase it
             }
         }
-        return false;
+
+        return false; // no valid pair found
     }
 };
